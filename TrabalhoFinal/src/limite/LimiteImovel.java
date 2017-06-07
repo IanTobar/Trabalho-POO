aba7dcca24c861a9ec4409ddbdf86a3ead9822f1
Unknown <ruanbento@gmail.com>
2017-06-07 20:42:14 -0300

    Adionada tela de cadastro de imoveis

--- TrabalhoFinal/src/limite/LimiteImovel.java
+++ TrabalhoFinal/src/limite/LimiteImovel.java
@@ -6,13 +6,16 @@
 package limite;
 
 import controle.*;
+
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 /**
  *
  * @author Ruan
  */
-public class LimiteImovel extends JFrame {
+public class LimiteImovel extends JFrame implements ActionListener {
 
     ControleImovel ctrImovel;
 
@@ -23,6 +26,7 @@ public class LimiteImovel extends JFrame {
     JButton btCadastrar;
     JButton btConsultar;
 
+    //labels
     JLabel lbOpcoes;
 
     public LimiteImovel(ControleImovel ctrImovel) {
@@ -51,6 +55,10 @@ public class LimiteImovel extends JFrame {
         btConsultar.setBounds(0, 50, 150, 25);//Posição
         btConsultar.setText("Consultar");
 
+        //Listeners dos botões
+        btCadastrar.addActionListener(this);
+        btConsultar.addActionListener(this);
+
         //Adição do conteudo no painel
         panel.add(btCadastrar);
         panel.add(btConsultar);
@@ -60,5 +68,115 @@ public class LimiteImovel extends JFrame {
         getContentPane().add(panel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(btCadastrar)) {
+            cadastraImovel();
+        }
+        if (e.getSource().equals(btConsultar)) {
+
+        }
+
+    }
+
+    void cadastraImovel() {
+        //Inicializa Tela
+        JFrame frCadastra = new JFrame();
+
+        //Inicializa Painel
+        JPanel pCadastro = new JPanel();
+
+        //Inicializa TextFields 
+        JTextField tfCodigo = new JTextField();
+        JTextField tfTipo = new JTextField();
+        JTextField tfDescricao = new JTextField();
+        JTextField tfProprietario = new JTextField();
+        JTextField tfPreco = new JTextField();
+        JTextField tfData = new JTextField();
+
+        //Inicializa Labels
+        JLabel lbLogo = new JLabel();
+        JLabel lbCodigo = new JLabel();
+        JLabel lbTipo = new JLabel();
+        JLabel lbDescricao = new JLabel();
+        JLabel lbProprietario = new JLabel();
+        JLabel lbPreco = new JLabel();
+        JLabel lbData = new JLabel();
+
+        //Inicializa Botões
+        JButton btSubmit = new JButton();
+        JButton btCancelar = new JButton();
+
+        
+        pCadastro.setLayout(null);
+
+        lbLogo.setText("DADOS DO IMOVEL");
+
+        lbCodigo.setText("Codigo:");
+        lbTipo.setText("Tipo:");
+        lbDescricao.setText("Descrição:");
+        lbProprietario.setText("Nome do proprietário:");
+        lbPreco.setText("Preço proposto:");
+        lbData.setText("Data do Cadastro:");
+
+        btSubmit.setText("Cadastrar");
+        btSubmit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Funciona!");
+            }
+        });
+        
+        btCancelar.setText("Cancelar");
+        btCancelar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frCadastra.dispose();
+            }
+        });
+
+        lbLogo.setBounds(200, 0, 125, 25);
+
+        lbCodigo.setBounds(0, 50, 500, 25);
+        lbTipo.setBounds(0, 100, 500, 25);
+        lbDescricao.setBounds(0, 150, 500, 25);
+        lbProprietario.setBounds(0, 200, 500, 25);
+        lbPreco.setBounds(0, 250, 500, 25);
+        lbData.setBounds(0, 300, 500, 25);
+
+        tfCodigo.setBounds(0, 75, 500, 25);
+        tfTipo.setBounds(0, 125, 500, 25);
+        tfDescricao.setBounds(0, 175, 500, 25);
+        tfProprietario.setBounds(0, 225, 500, 25);
+        tfPreco.setBounds(0, 275, 500, 25);
+        tfData.setBounds(0, 325, 500, 25);
+
+        btSubmit.setBounds(200, 375, 100, 25);
+        btCancelar.setBounds(200, 425, 100, 25);
+        
+        pCadastro.add(tfCodigo);
+        pCadastro.add(tfTipo);
+        pCadastro.add(tfDescricao);
+        pCadastro.add(tfProprietario);
+        pCadastro.add(tfPreco);
+        pCadastro.add(tfData);
+
+        pCadastro.add(lbLogo);
+        pCadastro.add(lbCodigo);
+        pCadastro.add(lbTipo);
+        pCadastro.add(lbDescricao);
+        pCadastro.add(lbProprietario);
+        pCadastro.add(lbPreco);
+        pCadastro.add(lbData);
+
+        pCadastro.add(btSubmit);
+        pCadastro.add(btCancelar);
+        
+        frCadastra.setSize(500, 500);
+        frCadastra.getContentPane().add(pCadastro);
+        frCadastra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frCadastra.setVisible(true);
     }
 }
